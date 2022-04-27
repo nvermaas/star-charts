ebe88859f093e5e23462d1d9f87b6bcdd568c5fd
Nico Vermaas <vermaas@astron.nl>
2022-04-27 17:29:48 +0200

    starting work, with Rob Dawson starcharts at the core

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 import sky_area
 from input_file import InputFile
 from diagram import Diagram
+from stardata import StarData
 from coord_calc import CoordCalc
 
 f = InputFile('stardata.csv')
 
-area = sky_area.SKY_AREA_ORION
+area = sky_area.HD84406
+
+databas = "hygdata.sqlite3"
+sd = StarData(database)
+star_data_list = sd.get_stars(area)
+
 star_data_list = f.get_stars(area)
 
 cc = CoordCalc(star_data_list, area, 500)
@@ -14,4 +20,4 @@ cc.process()
 
 d = Diagram('My Star Map', area, star_data_list)
 list(map(d.add_curve, cc.calc_curves()))
-d.render_svg('star-chart.svg')
+d.render_svg('hd84406.svg')
---
+++ stardata.py
@@ -0,0 +1,6 @@
+class StarData:
+    def __init__(self, database):
+        self.database = database
+
+    def get_stars(self, area):
+        return None
